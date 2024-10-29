85c2271f55da39245e96679ab1c5c678b9cc8172
arslan-barslan <arslanrulit28@gmail.com>
2024-10-29 18:04:06 +0500

    4 задание Ибрагимов

---
+++ ElementIbragimov/ElementIbragimov.py
@@ -0,0 +1,9 @@
+class ElementIbragimov():
+	def __init__(self, name, symbol,number):
+		self.name = name
+		self.symbol = symbol
+		self.number = number 
+
+elementik=ElementIbragimov('Бериллий','Be',4)
+
+
