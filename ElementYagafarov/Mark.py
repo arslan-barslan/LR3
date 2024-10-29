f0c56c191c11a12486549f57db5a35bd76a9fd08
Mark2816387 <markhwgcu3165@gmail.com>
2024-10-29 18:25:34 +0500

    Марк

---
+++ ElementYagafarov/Mark.py
@@ -0,0 +1,8 @@
+class ElementYagafarov():
+        def __init__(self, name, symbol, number):
+                self.name = name
+                self.symbol = symbol
+                self.number = number 
+
+element=ElementYagafarov('Хром','Cr','24')
+
