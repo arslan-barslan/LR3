7cb565ca10d74757161305299e3c69cc3c934426
AygizAsfandiyarov <aigizasfandiyarov@gmail.com>
2024-10-29 20:22:55 +0300

    Задание №5

---
+++ ElementAsfandiyarov/task5.py
@@ -0,0 +1,13 @@
+class ElementAsfandiyarov():
+        def __init__(self, name, symbol, number):
+                self.name = name
+                self.symbol = symbol
+                self.number = number
+	
+	def dump():
+		print('name:', self.name)
+		print('symbol:', self.symbol)
+		print('number:', self.number)
+
+element = ElementAsfandiyarov('Водород', 'H', 1)
+element.dump
