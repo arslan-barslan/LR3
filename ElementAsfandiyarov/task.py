288da6763e6d36e83de0d47d70c8b8edad81657e
AygizAsfandiyarov <aigizasfandiyarov@gmail.com>
2024-10-29 18:40:40 +0300

    Задание №4

---
+++ ElementAsfandiyarov/task.py
@@ -0,0 +1,7 @@
+class ElementAsfandiyarov():
+        def __init__(self, name, symbol, number):
+                self.name = name
+                self.symbol = symbol
+                self.number = number
+
+element = ElementAsfandiyarov('Водород', 'H', 1)
