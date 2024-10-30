ca06c411fc41c8af28776f8d1e29c31b0c32d972
Mark2816387 <markhwgcu3165@gmail.com>
2024-10-30 22:45:19 +0500

    fixs

--- app/app.py
+++ app/app.py
@@ -2,26 +2,24 @@ class Conus():
         def __init__(self,radius,height):
                 self.radius = radius
                 self.height = height
+
         @property
         def radius(self):
-                return self.radius
-        @property
-        def height(self):
-                return self.height
-    
-        @height.setter
-        def height(self, value):
-                if value <= 0:
-                        raise ValueError("Высота должна быть больше нуля, и не равнятся нулю")
-                self.height = value
-        
+                return self.__radius
         @radius.setter
         def radius(self, value):
                 if value <= 0:
-                        raise ValueError("Радиус должен быть больше нуля, и не равнятся нулю")
-                self.radius = value 
+                        raise ValueError("Радиус должен быть больше нуля")
+                self.__radius = value 
+        @property
+        def height(self):
+                return self.__height
+        @height.setter
+        def height(self, value):
+                if value <= 0:
+                        raise ValueError("Высота должна быть больше нуля")
+                self.__height = value
         
-	
         @staticmethod
         def about():
                 print("Данную работу сделали Ибрагимов А.А как разработчик №1, Ягафаров М.М как разработчик №2 и Асфандияров А.Р разработчик №3")
