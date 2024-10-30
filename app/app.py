9a240c1abdc30b7ec616797c42c92d196bb31bce
Arslan Ibragimov <arslanrulit28@gmail.com>
2024-10-30 21:17:23 +0500

    Задание 7

---
+++ app/app.py
@@ -0,0 +1,8 @@
+class Conus():
+	def __init__(self,radius,height):
+		self.radius = radius
+		self.height = height
+	
+	@staticmethod
+	def about():
+		print("Данную работу сделали Ибрагимов А.А как разработчик №1, Ягафаров М.М как разработчик №2 и Асфандияров А.Р разработчик №3")
